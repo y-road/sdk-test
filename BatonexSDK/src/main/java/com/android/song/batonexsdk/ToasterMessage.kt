72676c5b3a33340ca0fa71742a4ca5ed3aada125
y-road <shsong4193@gmail.com>
2024-12-27 16:58:56 +0900

    Fix: Change class to object

--- BatonexSDK/src/main/java/com/android/song/batonexsdk/ToasterMessage.kt
+++ BatonexSDK/src/main/java/com/android/song/batonexsdk/ToasterMessage.kt
@@ -3,7 +3,7 @@ package com.android.song.batonexsdk
 import android.content.Context
 import android.widget.Toast
 
-class ToasterMessage {
+object ToasterMessage {
 
     fun show(context: Context, message: String) {
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
