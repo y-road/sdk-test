f2dd9239173c6f86565a61f1c2bb57f80bee7283
y-road <shsong4193@gmail.com>
2024-12-27 16:51:13 +0900

    Fix: Add module implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,9 @@ android {
 
 dependencies {
 
+
+    implementation(project(":BatonexSDK"))
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
